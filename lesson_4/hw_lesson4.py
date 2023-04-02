81ff6ff9df833abf06fc84c2d47b99bc6c91d9bf
tatsianaivchik <tatsianaivchik@gmail.com>
2023-04-02 15:58:23 -0700

    Homework Lesson 4*

--- lesson_4/hw_lesson4.py
+++ lesson_4/hw_lesson4.py
@@ -8,13 +8,15 @@ def even_first(arr):
         if arr[even_index] % 2 == 0:
             even_index += 1
         else:
-            arr[odd_index], arr[even_index] = arr[even_index], arr[odd_index]
+            arr[even_index], arr[odd_index] = arr[odd_index], arr[even_index]
             odd_index -= 1
-
     return arr
 
 test_arr = [7, 3, 5, 6, 4, 10, 3, 2]
-print(even_first(test_arr))
+test_arr2 = [5, 3, 4, 6, 10, 9, 11]
+#
+# print(even_first(test_arr))
+# print(even_first(test_arr2))
 
 
 # Write a program that takes as input a list of digits encoding a nonnegative decimal integer D and updates the list to represent the integer D + 1.
@@ -22,19 +24,26 @@ print(even_first(test_arr))
 
 
 def plus_one(digits):
-    carry = 1
-    for i in range(len(digits)-1, -1, -1):
-        digits[i] += carry
-        if digits[i] == 10:
-            digits[i] = 0
-            carry = 1
-        else:
-            carry = 0
-            break
-    if carry == 1:
-        digits.insert(0, 1)
-    return digits
+    new_dun = "".join(str(x) for x in digits)
+    new_dun = str(int(new_dun) + 1)
+    return [i for i in new_dun]
 
-
+# second approach
+#     carry = 1
+#     for i in range(len(digits)-1, -1, -1):
+#         digits[i] += carry
+#         if digits[i] == 10:
+#             digits[i] = 0
+#             carry = 1
+#         else:
+#             carry = 0
+#             break
+#     if carry == 1:
+#         digits.insert(0, 1)
+#     return digits
+#
+#
 digits_test = [1, 2, 9]
+digits_test2 = [1, 8, 9, 1]
 print(plus_one(digits_test))
+print(plus_one(digits_test2))
